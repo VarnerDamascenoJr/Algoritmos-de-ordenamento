eca68c59a7fff17745ef7fa72090e14a5fb8f40f
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-06-10 21:10:42 -0300

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,13 @@
+*Código ainda nao terminado*****
+vec = [1,2,454,65,7,98,6,4,3]
+i = 0
+j = 0
+k = 0
+aux= 0
+for i in range(0,vec[])
+   print(vec[i])
+   
+*bubble sort*
+for i in range(0,vec[i]):
+   for j in range(0, vec[i] - 1):
+      if(vec[i]<vec[j):
