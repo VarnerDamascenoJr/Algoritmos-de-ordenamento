14b14bec3a56072a76e4f33342cb56464aeddae6
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-09 17:23:31 -0500

    Implementação do merge-Sort
    Ainda não está completo, embora há falta de pouca coisa. A saber disso, ainda falta um pouco de tratamento e a implementação dos vetores.

--- Merge_Sort/merge_Sort.java
+++ Merge_Sort/merge_Sort.java
@@ -1,9 +1,31 @@
-public static void mergeSort(int[]a, int n){
-   if(n< 2){return 0;}
-   
-   int mind = n/2;
-   int[]l   = new int[mind];
-   int[]r   = new int[n - mind];
-   for(int i = 0; i< mind; i++){l[i] = a[i];}
-   for(int i; i < n; i++){r[i - mind] = a[i];}
+//ainda está faltando algumas partes. Falta um tratamento em if.
+
+package MergeSort;
+
+public class Principal {
+    public static void main(String[] args){
+      int[]v = {4,6,7,3,};
+    }
+}
+private static void mergeSort(int[]v, int[]w, int init, int fim ){
+    if(init< fim){
+     int meio = (init+ fim)/2;
+     mergeSort(v, w, init, meio);
+     mergeSort(v, w, meio+1, fim);
+     intercalar(v, w, init, meio, fim);
+    }
+}
+
+private static void intercalar(int[]v, int[]w, int init, int meio, int fim){
+    for (int k= init; ḱ <=fim;k++){
+        w[k] = v[k];
+    }
+   int i = init;
+   int j = meio+1;
+    for (int k =init; k<=fim; k++){
+        if (i > meio)v[k] = w[j++];
+        else if (j > fim) v[k] = w[i + 1];
+        else if (w[i] < w[j]) v[k] = w[i];
+
+    }
 }
